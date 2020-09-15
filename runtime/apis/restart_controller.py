aba1a7e7f79ea62a449a12c03bd2faf4fcdda47a
RamsayLouw <ramsay@ecosystem.ai>
2020-09-15 02:06:07 +0200

    Update restart_controller.py

--- runtime/apis/restart_controller.py
+++ runtime/apis/restart_controller.py
@@ -1,15 +1,15 @@
 from runtime.endpoints import restart_controller as endpoints
 from runtime import request_utils
 
-# Restart the server
 def restart(auth):
+# Restart the server
     ep = endpoints.RESTART
     resp = request_utils.create(auth, ep)
     meta = resp.json()
     return meta
 
-# Restart using actuator
 def restart_app(auth):
+# Restart using actuator
     ep = endpoints.RESTART_APP
     resp = request_utils.create(auth, ep)
     meta = resp.json()
