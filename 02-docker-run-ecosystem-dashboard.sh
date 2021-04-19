1b5c7f47ea9dc68f75258227a519478dc13cc299
jayvanzyl <jayvanzyl@gmail.com>
2021-04-19 17:09:51 +0200

    update

---
+++ 02-docker-run-ecosystem-dashboard.sh
@@ -0,0 +1,3 @@
+# docker run -d -p 5000:5000 ecosystemai/ecosystem-dashboard
+docker run --rm -it -p 5020:5000 --net-alias ecosystem-dashboard --network ecosystem ecosystemai/ecosystem-dashboard
+
--- 02-docker-run.sh
+++
@@ -1,2 +0,0 @@
-# docker run -d -p 5000:5000 ecosystemai/ecosystem-dashboard
-docker run -it -p 5000:5000 --net-alias ecosystem-notebooks --network ecosystem ecosystemai/ecosystem-notebooks
